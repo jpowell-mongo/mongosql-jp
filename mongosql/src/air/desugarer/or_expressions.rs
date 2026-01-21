e67d81eb5751b1e0e13544933e3a2fffd7c82f40
Jonathan Powell <jonathan.powell@mongodb.com>
2026-01-21 11:40:05 -0500

    Restored index usage for nullable fields. More testing TODOs

--- mongosql/src/air/desugarer/or_expressions.rs
+++ mongosql/src/air/desugarer/or_expressions.rs
@@ -1,5 +1,6 @@
 use crate::air;
 use crate::air::desugarer::Pass;
+use crate::air::util::sql_op_to_mql_op;
 use crate::air::visitor::Visitor;
 use crate::air::{
     Expression, Expression::*, MqlOperator, MqlSemanticOperator, SqlOperator, SqlSemanticOperator,
@@ -12,7 +13,7 @@ pub struct OrExpressionsDesugarerPass;
 impl Pass for OrExpressionsDesugarerPass {
     fn apply(&self, pipeline: Stage) -> air::desugarer::Result<Stage> {
         let visitor = &mut OrExpressionsDesugarerVisitor {
-            in_or_context: false,
+            is_within_or_context: false,
         };
         let stage = pipeline.walk(visitor);
         Ok(stage)
@@ -21,13 +22,21 @@ impl Pass for OrExpressionsDesugarerPass {
 
 #[derive(Default)]
 struct OrExpressionsDesugarerVisitor {
-    in_or_context: bool,
+    is_within_or_context: bool,
 }
 
 impl OrExpressionsDesugarerVisitor {
-    fn desugar_or_expression(&self, operator: SqlSemanticOperator) -> Expression {
+    fn desugar_sql_semantic_operator_expression(
+        &self,
+        operator: SqlSemanticOperator,
+    ) -> Expression {
+        let as_mql_op = sql_op_to_mql_op(operator.op);
+        if !self.is_within_or_context || as_mql_op.is_none() {
+            return SqlSemanticOperator(operator);
+        }
+
         MqlSemanticOperator(MqlSemanticOperator {
-            op: MqlOperator::Or,
+            op: as_mql_op.unwrap(),
             args: operator.args,
         })
     }
@@ -35,16 +44,25 @@ impl OrExpressionsDesugarerVisitor {
 
 impl Visitor for OrExpressionsDesugarerVisitor {
     fn visit_expression(&mut self, node: Expression) -> Expression {
+        // TODO: We want to re-write ALL sqlSemantic operators to mqlSemantic operators that are children in an or tree.
         let node = match node {
             SqlSemanticOperator(s) => match s.op {
                 SqlOperator::Or => {
-                    self.in_or_context = true;
-                    self.desugar_or_expression(s)
+                    self.is_within_or_context = true;
+                    let n = self.desugar_sql_semantic_operator_expression(s);
+                    let completed_tree = n.walk(self);
+                    self.is_within_or_context = false;
+                    completed_tree
                 }
-                _ => SqlSemanticOperator(s),
+                _ => {
+                    let n = self.desugar_sql_semantic_operator_expression(s);
+                    let completed_tree = n.walk(self);
+                    completed_tree
+                }
             },
+            // Add SubqueryComparison and Exists here
             Subquery(s) => {
-                self.in_or_context = false;
+                self.is_within_or_context = false;
                 Subquery(s)
             }
             _ => node,
