3afca96325f676ff094d7ec2d8dbb9910c1c5007
Jonathan Powell <jonathan.powell@mongodb.com>
2026-01-22 12:50:42 -0500

    Refactor to only target match stage. Validate transformation only happens in match stage

--- mongosql/src/air/desugarer/or_expressions.rs
+++ mongosql/src/air/desugarer/or_expressions.rs
@@ -3,20 +3,42 @@ use crate::air::desugarer::Pass;
 use crate::air::util::sql_op_to_mql_op;
 use crate::air::visitor::Visitor;
 use crate::air::{
-    Expression, Expression::*, MqlOperator, MqlSemanticOperator, SqlOperator, SqlSemanticOperator,
-    Stage,
+    Expression, Expression::*, Match, MatchLanguage, MqlSemanticOperator, SqlOperator,
+    SqlSemanticOperator, Stage,
 };
 
-// Notes: Do I want to keep track of the scope level as I traverse the tree?
 pub struct OrExpressionsDesugarerPass;
 
 impl Pass for OrExpressionsDesugarerPass {
     fn apply(&self, pipeline: Stage) -> air::desugarer::Result<Stage> {
-        let visitor = &mut OrExpressionsDesugarerVisitor {
-            is_within_or_context: false,
-        };
+        let visitor = &mut FilterStageDesugarerVisitor {};
         let stage = pipeline.walk(visitor);
         Ok(stage)
+    }
+}
+
+#[derive(Default)]
+struct FilterStageDesugarerVisitor {}
+
+impl FilterStageDesugarerVisitor {}
+impl Visitor for FilterStageDesugarerVisitor {
+    fn visit_match(&mut self, node: Match) -> Match {
+        let node: Match = match node {
+            Match::MatchLanguage(m) => {
+                let or_expression_visitor = &mut OrExpressionsDesugarerVisitor::default();
+                let visited = m.expr.walk(or_expression_visitor);
+                Match::MatchLanguage(MatchLanguage {
+                    source: m.source,
+                    expr: Box::new(visited),
+                })
+            }
+            Match::ExprLanguage(e) => {
+                let or_expression_visitor = &mut OrExpressionsDesugarerVisitor::default();
+                let visited = e.walk(or_expression_visitor);
+                Match::ExprLanguage(visited)
+            }
+        };
+        node.walk(self)
     }
 }
 
@@ -44,20 +66,18 @@ impl OrExpressionsDesugarerVisitor {
 
 impl Visitor for OrExpressionsDesugarerVisitor {
     fn visit_expression(&mut self, node: Expression) -> Expression {
-        // TODO: We want to re-write ALL sqlSemantic operators to mqlSemantic operators that are children in an or tree.
         let node = match node {
             SqlSemanticOperator(s) => match s.op {
                 SqlOperator::Or => {
                     self.is_within_or_context = true;
-                    let n = self.desugar_sql_semantic_operator_expression(s);
-                    let completed_tree = n.walk(self);
+                    let desugared_parent = self.desugar_sql_semantic_operator_expression(s);
+                    let desugared_tree = desugared_parent.walk(self);
                     self.is_within_or_context = false;
-                    completed_tree
+                    desugared_tree
                 }
                 _ => {
-                    let n = self.desugar_sql_semantic_operator_expression(s);
-                    let completed_tree = n.walk(self);
-                    completed_tree
+                    let desugared_parent = self.desugar_sql_semantic_operator_expression(s);
+                    desugared_parent.walk(self)
                 }
             },
             // Add SubqueryComparison and Exists here
@@ -65,6 +85,16 @@ impl Visitor for OrExpressionsDesugarerVisitor {
                 self.is_within_or_context = false;
                 Subquery(s)
             }
+            SubqueryComparison(s) => {
+                self.is_within_or_context = false;
+                SubqueryComparison(s)
+            }
+            // Note: I couldn't seem to get exists operator to match here?
+            SubqueryExists(s) => {
+                self.is_within_or_context = false;
+                SubqueryExists(s)
+            }
+
             _ => node,
         };
 
